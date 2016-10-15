ebe77d53f721756501bbf51dd43b3495caccb53d
liyingxuan <44600937@qq.com>
2016-10-15 09:10:17 +0800

    debug

--- app/Api/Helper/WeiXinPay.php
+++ app/Api/Helper/WeiXinPay.php
@@ -57,8 +57,9 @@ class WeiXinPay
             'trade_type' => 'APP'
         );
         $data['sign'] = $this->wxMd5Sign($data);
-        $data = $this->wxArrayToXml($data);
-//        file_put_contents('pay.file', json_encode($data));
+        $dataXml = $this->wxArrayToXml($data);
+        return ['test' => $data, 'test1' => $dataXml];
+//        file_put_contents('pay.file', json_encode($dataXml));
 
         $ch = curl_init();
         curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 30000);
@@ -68,7 +69,7 @@ class WeiXinPay
         curl_setopt($ch, CURLOPT_HEADER, FALSE);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
         curl_setopt($ch, CURLOPT_POST, TRUE);
-        curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $dataXml);
         $return = curl_exec($ch);
         curl_close($ch);
 
