463a536239db3d7d2a70ec58a28226d2e2810ad9
liyingxuan <44600937@qq.com>
2016-10-17 17:33:56 +0800

    style="width:100%;"

--- resources/views/about/introduction.blade.php
+++ resources/views/about/introduction.blade.php
@@ -5,6 +5,6 @@
     <meta content='width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no' name='viewport'>
 </head>
 <body>
-    <p style="text-align:center; width: 100%;"> <img src="/about/introduction.png"> </p>
+    <p style="text-align:center; width: 100%;"> <img style="width:100%;" src="/about/introduction.png"> </p>
 </body>
 </html>
