ee28e95d8b10355b56aca6c0a603c4dd51d4f19d
liyingxuan <44600937@qq.com>
2017-01-21 22:19:51 +0800

    增加日志文件的访问权限变更

--- app/Http/Controllers/LogController.php
+++ app/Http/Controllers/LogController.php
@@ -10,6 +10,8 @@ class LogController extends Controller
         $fileName = $dir . 'laravel-' . date('Y-m-d', time()) . '.log';
 
         if (file_exists($fileName)) {
+            chmod($fileName, 0777); //修改文件的权限，防止被访问之后无法使用；权限用的八进制
+
             $content = file_get_contents($fileName);
             $data['content'] = dump($content);
         } else {
