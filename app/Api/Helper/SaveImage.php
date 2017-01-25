1775ed638f76d5831cded2089182222c9743e397
lyx <44600937@qq.com>
2017-01-25 12:56:08 +0800

    Update save image helper.

--- app/Api/Helper/SaveImage.php
+++ app/Api/Helper/SaveImage.php
@@ -52,7 +52,7 @@ class SaveImage
         $suffix = '.png';
         $filename = time() + $count . $suffix;
         $fullPath = $destinationPath . $filename;
-        $newPath = str_replace($suffix, '_thumb.png', $fullPath);
+        $newPath = str_replace($suffix, '_thumb' . $suffix, $fullPath);
 
         try {
             $imgFile->move($destinationPath, $filename);
@@ -78,7 +78,7 @@ class SaveImage
         $suffix = '.png';
         $filename = time() . $suffix;
         $fullPath = $destinationPath . $filename;
-        $newPath = str_replace($suffix, '_thumb.png', $fullPath);
+        $newPath = str_replace($suffix, '_thumb' . $suffix, $fullPath);
 
         try {
             $file->move($destinationPath, $filename);
@@ -88,5 +88,75 @@ class SaveImage
         }
 
         return $domain . '/' . $newPath;
+    }
+
+    /**
+     * 保存Banner图片
+     *
+     * @param $file
+     * @return string
+     */
+    public static function banner($file)
+    {
+        //文件是否上传成功
+        if ($file->isValid()) {    //判断文件是否上传成功
+//            $originalName = $file->getClientOriginalName(); //源文件名
+//            $ext = $file->getClientOriginalExtension();    //文件拓展名
+//            $type = $file->getClientMimeType(); //文件类型
+
+            $domain = \Config::get('constants.DOMAIN');
+            $destinationPath = \Config::get('constants.BANNER_PATH');
+            $suffix = '.png';
+            $filename = date('YmdHis') . $suffix;
+            $fullPath = $destinationPath . $filename;
+            $newPath = str_replace($suffix, '_thumb' . $suffix, $fullPath);
+
+            try {
+                $file->move($destinationPath, $filename);
+                Image::make($fullPath)->encode('png', 30)->save($newPath);
+            } catch (\Exception $e) {
+                Log::info('save-img-banner', ['context' => $e->getMessage()]);
+            }
+
+
+            return $domain . $newPath;
+        } else {
+            return '';
+        }
+    }
+
+    /**
+     * 保存广播图片
+     *
+     * @param $file
+     * @return string
+     */
+    public static function radio($file)
+    {
+        //文件是否上传成功
+        if ($file->isValid()) {    //判断文件是否上传成功
+//            $originalName = $file->getClientOriginalName(); //源文件名
+//            $ext = $file->getClientOriginalExtension();    //文件拓展名
+//            $type = $file->getClientMimeType(); //文件类型
+
+            $domain = \Config::get('constants.DOMAIN');
+            $destinationPath = \Config::get('constants.ARTICLE_PATH');
+            $suffix = '.png';
+            $filename = date('YmdHis') . $suffix;
+            $fullPath = $destinationPath . $filename;
+            $newPath = str_replace($suffix, '_thumb' . $suffix, $fullPath);
+
+            try {
+                $file->move($destinationPath, $filename);
+                Image::make($fullPath)->encode('png', 30)->save($newPath);
+            } catch (\Exception $e) {
+                Log::info('save-img-radio', ['context' => $e->getMessage()]);
+            }
+
+
+            return $domain . $newPath;
+        } else {
+            return '';
+        }
     }
 }
