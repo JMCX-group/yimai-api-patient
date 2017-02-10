cb84f43f05475cf59593f00ce529d5db6a42e9c1
lyx <44600937@qq.com>
2017-02-10 18:02:53 +0800

    修改文案

--- app/Api/Transformers/TimeLineTransformer.php
+++ app/Api/Transformers/TimeLineTransformer.php
@@ -478,7 +478,7 @@ class TimeLineTransformer
             'content' => $supplement
         ], [
             'name' => \Config::get('constants.TREATMENT_NOTICE'),
-            'content' => $appointments->remark
+            'content' => ($appointments->remark == null) ? '无' : $appointments->remark
         ]];
     }
 
